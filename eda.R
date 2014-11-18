4d777f46a1b50b50d87e560fb35a40e5ba01baad
Kenny Joseph <josephkena@gmail.com>
2014-11-17 19:55:46 -0500

    fixes, plot changes, actually uploading the R code

--- eda.R
+++ eda.R
@@ -8,19 +8,7 @@ library(mgcv)
 setwd("~/git/TagsAndMemory/")
 source("functions.R")
 
-plot_labels <- c("6M before peak",
-            "5M before peak",
-            "4M before peak",
-            "3M before peak",
-            "2M before peak",
-            "1M before peak",
-            "Peak",
-            "1M after peak",
-            "2M after peak",
-            "3M after peak",
-            "4M after peak",
-            "5M after peak",
-            "6M after peak")
+plot_labels <- -6:6
 
 before_cols <- paste0("V",4:9)
 after_cols <- paste0("V",11:16)
@@ -71,9 +59,9 @@ p_eda <- p_eda + facet_wrap(~variable,scales="free_y") + scale_x_log10()
 
 ##the distributions are pretty skewed, so lets double check using logs....same thing. 
 normal_ci <- data_melt[,list(mean=mean(value),
-                                 median=median(value),
-                                 se=sd(value)/sqrt(length(id))),
-                            by=c("tagged","variable")]
+                             median=median(value),
+                             se=sd(value)/sqrt(length(id))),
+                       by=c("tagged","variable")]
 setnames(normal_ci,"variable","month")
 normal_ci[,mean_norm:=mean/max(mean),by=tagged]
 normal_ci[,se_norm:=se/max(mean),by=tagged]
@@ -83,10 +71,10 @@ p1 <- ggplot(normal_ci, aes(x=month,y=mean_norm,
                             linetype=tagged, group=tagged))
 p1 <- p1 + geom_line(size=.8)
 p1 <- p1 + geom_pointrange(aes(x=month,
-                              ymin=mean_norm-1.96*se_norm,
-                              ymax=mean_norm+1.96*se_norm)) 
-p1 <- p1 + scale_x_discrete(labels=plot_labels) + theme(axis.text.x=element_text(angle=45,hjust=1)) + scale_linetype_discrete("Tagged",labels=c("No","Yes")) + xlab("Month") + ylab("Mean Listens\n(normalized)")
-
+                               ymin=mean_norm-1.96*se_norm,
+                               ymax=mean_norm+1.96*se_norm)) 
+p1 <- p1 + scale_x_discrete(labels=plot_labels) + scale_linetype_discrete("Tagged",labels=c("No","Yes")) + xlab("Months Before/After Peak (at 0)") + ylab("Mean Listens (normalized)")
+ggsave("paper/taggedVUntaggedSimple.png",p1, dpi=400,w=10,h=6)
 ############################
 #####REGRESSION ANALYSIS####
 ############################
@@ -95,8 +83,9 @@ p1 <- p1 + scale_x_discrete(labels=plot_labels) + theme(axis.text.x=element_text
 reg_data <- data[log(V10) < 7.5]
 reg_data$tagged <- factor(reg_data$tagged)
 
-ggplot() + stat_smooth(data=reg_data, aes(log(V10), log(after_sum), linetype=tagged),method='gam',formula=y~s(x)) + xlab("Log of listens in peak month") + ylab("Log of listens in\nfollowing 6 months") + scale_linetype_discrete("Tagged",labels=c("No","Yes"))
 
+p2 <- ggplot() + stat_smooth(data=reg_data, aes(V10, after_sum, linetype=tagged),method='gam',formula=y~s(x)) + xlab("Listens in peak month") + ylab("Listens in\nfollowing 6 months") + scale_linetype_discrete("Tagged",labels=c("No","Yes")) + scale_x_log10() + scale_y_log10()
+ggsave("paper/taggedVUntaggedRegression.png",p2, dpi=400,w=10,h=6)
 
 res_gam <- gam(log(after_sum)~s(log(V10))+s(log(before_sum))+tagged,data=reg_data)
 res_gam2 <- gam(log(after_sum)~s(log(V10))+tagged,data=reg_data)
@@ -118,17 +107,52 @@ vis.gam(res_gam3)
 #########HYPOTHESIS 2##########
 ###############################
 
+tagged <- get_data("data/tagged.csv",
+                   before_cols,after_cols,all_cols)
+
+tagged_used <- tagged[V10 >= V4 &
+                        V10 >= V5 &
+                        V10 >= V6 &
+                        V10 >= V7 &
+                        V10 >= V8 &
+                        V10 >= V9 &
+                        V10 >= V11 &
+                        V10 >= V12 &
+                        V10 >= V13 &
+                        V10 >= V14 &
+                        V10 >= V15 &
+                        V10 >= V16,]
+dup <- which(!duplicated(tagged_used,by=c("V1","V2")))
+tagged_used <- tagged_used[dup,]
+
+tagged_not_used <- tagged[!(V10 >= V4 &
+                              V10 >= V5 &
+                              V10 >= V6 &
+                              V10 >= V7 &
+                              V10 >= V8 &
+                              V10 >= V9 &
+                              V10 >= V11 &
+                              V10 >= V12 &
+                              V10 >= V13 &
+                              V10 >= V14 &
+                              V10 >= V15 &
+                              V10 >= V16),]
+dup <- which(!duplicated(tagged_not_used,by=c("V1","V2")))
+tagged_not_used <- tagged_not_used[dup,]
+
+tags_for_reg <- rbind(tagged_used,tagged_not_used)
+
 ##controlling for previous listening behavior, which tags ultimately best predict future listening behavior
-setnames(tagged, c("V1","V2","V3"), c("user","artist","tag"))
-tag_counts <- tagged[,length(user),by=c("tag")]
+setnames(tags_for_reg, c("V1","V2","V3"), c("user","artist","tag"))
+tag_counts <- tags_for_reg[,length(user),by=c("tag")]
 
 tag_names <- fread("data/tag_names.tsv")
 setnames(tag_names,c("tag_id","name","ent1","ent2"))
 
-tagged_pred <- tagged[tag %in% tag_counts[V1 > 10,]$tag,]
+tagged_pred <- tags_for_reg[tag %in% tag_counts[V1 > 5,]$tag,]
 tagged_pred$tag_factor <- factor(tagged_pred$tag)
 
-res_tag <- bam(log(after_sum)~s(log(V10),bs="cr")+tag_factor,data=tagged_pred)
+res_tag <- bam(log(after_sum)~s(log(V10+.01),bs="cr")+tag_factor,data=tagged_pred)
 
 library(stringr)
 sum_res_tag <- summary(res_tag)
@@ -160,8 +184,42 @@ global_freq[Estimate < -.5 ]$type <- "Sig., x<-.5"
 global_freq[Estimate > -.5 & Estimate < 0 ]$type <- "Sig.,-.5<x<0"
 global_freq[Estimate > 0 & Estimate < .5 ]$type <- "Sig.,0<x<.5"
 
-global_freq$type <- factor(global_freq$type, levels=)
+
+global_freq$type <- factor(global_freq$type, levels=rev(c("Not sig.",
+                                                          "Sig., x<-.5",
+                                                          "Sig.,-.5<x<0",
+                                                          "Sig.,0<x<.5",
+                                                          "Sig., x>.5")), order=T)
+
 boot_res <- global_freq[,as.list(smean.cl.boot(freq)),by=type]
 
-ggplot(boot_res, aes(type,Mean,ymin=Lower,ymax=Upper, color=type=="Not sig.")) + geom_pointrange() + scale_y_log10() + coord_flip() + ylab("Frequency of tagging") + xlab("Regression Result") + scale_color_discrete("",labels=c("Significant","Not significant")) 
+p3 <-ggplot(global_freq,aes(Estimate,freq)) + geom_point() +  scale_y_log10() + ylab("Global Popularity") + xlab("Regression Coefficient") + geom_hline(y=11064.2, color='red') + geom_hline(y=15641, color='red')
+ggsave("paper/tagRegressionWithMoreData.png", dpi=400,w=10,h=6)
+
+
+
+
+
+user_freq <- fread("userCountsByTag.tsv")
+setnames(user_freq, c("tag_id","tag_name","freq"))
+user_freq <- merge(user_freq,all_sig_coeff,by="tag_id",all.x=T)
+user_freq <- user_freq[tag_id %in% tag_counts[count > 5,]$tag_id]
+user_freq$type <- "Not sig."
+user_freq[Estimate > .5 ]$type <- "Sig., x>.5"
+user_freq[Estimate < -.5 ]$type <- "Sig., x<-.5"
+user_freq[Estimate > -.5 & Estimate < 0 ]$type <- "Sig.,-.5<x<0"
+user_freq[Estimate > 0 & Estimate < .5 ]$type <- "Sig.,0<x<.5"
+
+
+user_freq$type <- factor(user_freq$type, levels=rev(c("Not sig.",
+                                                          "Sig., x<-.5",
+                                                          "Sig.,-.5<x<0",
+                                                          "Sig.,0<x<.5",
+                                                          "Sig., x>.5")), order=T)
+
+boot_res <- user_freq[,as.list(smean.cl.boot(freq)),by=type]
+
+p4 <-ggplot(user_freq,aes(Estimate,freq)) + geom_point() +  scale_y_log10() + ylab("Number of unique users") + xlab("Regression Coefficient") + geom_hline(y=671.2, color='red') + geom_hline(y=835.47, color='red')
+ggsave("paper/tagRegressionWithMoreDataPeople.png", dpi=400,w=10,h=6)
+
 
